310f05c3329dfdfc9a0c83c9a96993aa9cfb87b7
nora m <nora.ma@kellogg.northwestern.edu>
2024-06-04 02:53:47 +0000

    city created

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,3 +14,5 @@ class EntriesController < ApplicationController
   end
 
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,4 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
+
